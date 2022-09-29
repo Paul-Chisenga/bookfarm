c0362328b47e0edfb7dd6aa5b8d4f75506c9431e
paul <paulsther@gmail.com>
2022-09-30 01:13:43 +0300

    complete assignment

--- src/main/java/com/apps/bookfarm/controller/BookController.java
+++ src/main/java/com/apps/bookfarm/controller/BookController.java
@@ -1,43 +1,61 @@
 package com.apps.bookfarm.controller;
 
-import com.apps.bookfarm.repository.BookRepository;
+import com.apps.bookfarm.model.Author;
 import com.apps.bookfarm.model.Book;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
+import com.apps.bookfarm.service.BookService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@RequestMapping("/books")
 public class BookController {
-    private final BookRepository bookRepository;
+    private final BookService bookService;
 
-    public BookController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
     }
 
-    @GetMapping("/books/{id}")
-    EntityModel<Book> oneBook (@PathVariable Long id){
-        Book book = bookRepository.findById(id).orElseThrow(IllegalArgumentException::new);
-
-        return EntityModel.of(book,
-                linkTo(methodOn(BookController.class).oneBook(id)).withSelfRel(),
-                linkTo(methodOn(BookController.class).allBooks()).withRel("Books"));
+    @GetMapping("{id}")
+    Book getBook(@PathVariable Long id) {
+        return bookService.getBook(id);
     }
 
-    @GetMapping("/books")
-    CollectionModel<EntityModel<Book>> allBooks (){
-        List<EntityModel<Book>> books = bookRepository.findAll().stream()
-                .map(book -> EntityModel.of(book,
-                        linkTo(methodOn(BookController.class).oneBook(book.getBookId())).withSelfRel(),
-                        linkTo(methodOn(BookController.class).allBooks()).withRel("Books"))).collect(Collectors.toList());
+    @GetMapping
+    List<Book> getBooks() {
+        return bookService.getBooks();
+    }
 
-        return CollectionModel.of(books,
-                linkTo(methodOn(BookController.class).allBooks()).withSelfRel());
+    @PostMapping
+    String addNewBook(@RequestBody Book book) {
+        return bookService.addNewBook(book);
+    }
+
+    @PutMapping("{id}")
+    String updateBook(
+            @PathVariable("id") Long id,
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) Author author,
+            @RequestParam(required = false) String publisher,
+            @RequestParam(required = false) Integer isbn,
+            @RequestParam(required = false) Integer length,
+            @RequestParam(required = false) String subjects) {
+        return bookService.updateBook(id, title, author, publisher, isbn, length, subjects);
+    }
+
+    @DeleteMapping("{id}")
+    String deleteBook(@PathVariable("id") Long id) {
+        return bookService.deleteBook(id);
     }
 }
